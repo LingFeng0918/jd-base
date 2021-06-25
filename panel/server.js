79ca9f74c97f25f2a3575b858f76ed5a592a0cae
LingFeng <lingfeng_0918@163.com>
2021-06-25 21:24:58 +0800

    update

--- panel/server.js
+++ panel/server.js
@@ -91,7 +91,7 @@ async function step1() {
                 'Accept': 'application/json, text/plain, */*',
                 'Accept-Language': 'zh-cn',
                 'Referer': 'https://plogin.m.jd.com/login/login?appid=300&returnurl=https://wq.jd.com/passport/LoginRedirect?state=' + timeStamp + '&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+                'User-Agent': 'jdapp;android;9.3.5;10;2346663656561603-4353564623932316;network/wifi;model/ONEPLUS A5010;addressid/138709979;aid/2dfceea045ed292a;oaid/;osVer/29;appBuild/86390;partner/jingdong;eufv/1;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36',
                 'Host': 'plogin.m.jd.com'
             }
         });
@@ -314,13 +314,13 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ttyd proxy
-app.use('/shell', createProxyMiddleware({
-    target: 'http://localhost:7681',
-    ws: true,
-    changeOrigin: true,
+app.use('/shell', createProxyMiddleware({ 
+    target: 'http://localhost:7681', 
+    ws: true, 
+    changeOrigin: true, 
     pathRewrite: {
-        '^/shell': '/',
-    },
+        '^/shell': '/', 
+    }, 
 }));
 
 /**
@@ -348,7 +348,7 @@ app.get('/changepwd', function (request, response) {
 /**
  * terminal
  */
-app.get('/terminal', function (request, response) {
+ app.get('/terminal', function (request, response) {
     if (request.session.loggedin) {
         response.sendFile(path.join(__dirname + '/public/terminal.html'));
     } else {
@@ -752,7 +752,7 @@ app.get('/api/scripts', function (request, response) {
                 if (excludeRegExp.test(fileList[i])) {
                     continue;
                 }
-
+                
                 var dirMap = {
                     dirName: fileList[i],
                     files: fileListTmp
@@ -762,7 +762,7 @@ app.get('/api/scripts', function (request, response) {
                 if (excludeRegExp.test(fileList[i])) {
                     continue;
                 }
-
+                
                 rootFiles.push(fileList[i]);
             }
         }
