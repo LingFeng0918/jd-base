ece0f89f2ee07757bb369f5f18edfc4f28aff768
LingFeng <lingfeng_0918@163.com>
2021-12-27 11:12:42 +0800

    update

--- jcode.sh
+++ jcode.sh
@@ -21,7 +21,9 @@ gen_pt_pin_array () {
         tmp1=Cookie$user_num
         tmp2=${!tmp1}
         i=$(($user_num - 1))
+        pt_pin_temp_noturn=$(echo $tmp2 | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|}")
         pt_pin_temp=$(echo $tmp2 | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|; s|%|\\\x|g}")
+        [[ $pt_pin_temp_noturn == *\\x* ]] && pt_pin_test[i]=$(printf $pt_pin_temp_noturn) || pt_pin_test[i]=$pt_pin_temp_noturn
         [[ $pt_pin_temp == *\\x* ]] && pt_pin[i]=$(printf $pt_pin_temp) || pt_pin[i]=$pt_pin_temp
     done
 }
