fe1a9eb86b1cff7b3bbc2d732689726ccdc3c7fc
LingFeng9527 <87851165+LingFeng9527@users.noreply.github.com>
2022-04-02 01:37:33 +0800

    Update jup.sh

--- jup.sh
+++ jup.sh
@@ -4,8 +4,8 @@
 #dir_shell=$(dirname $(readlink -f "$0"))
 dir_shell=${JD_DIR:-$(cd $(dirname $0); pwd)}
 dir_root=$dir_shell
-url_shell=${JD_SHELL_URL:-https://ghproxy.com/https://github.com/LingFeng0918/jd-base.git}
-url_scripts=${JD_SCRIPTS_URL:-https://ghproxy.com/https://github.com/LingFeng0918/LF_JD.git}
+url_shell=${JD_SHELL_URL:-https://gh.fakev.cn/LingFeng0918/jd-base.git}
+url_scripts=${JD_SCRIPTS_URL:-https://gh.fakev.cn/LingFeng0918/LF_JD.git}
 send_mark=$dir_shell/send_mark
 
 ## 导入通用变量与函数
@@ -501,7 +501,7 @@ update_scripts () {
          rm -rf $dir_scripts
     fi
 
-    url_scripts=${JD_SCRIPTS_URL:-https://ghproxy.com/https://github.com/LingFeng0918/LF_JD.git}
+    url_scripts=${JD_SCRIPTS_URL:-https://gh.fakev.cn/LingFeng0918/LF_JD.git}
     branch_scripts=${JD_SCRIPTS_BRANCH:-main}
 
     ## 更新或克隆scripts
